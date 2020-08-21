3bbd76073e8aed60dbc4f0e851f3eef49376dca8
Esteban Santiesteban <me@estebansanti.com>
2020-08-21 19:09:43 -0400

    misc

--- README.md
+++ README.md
@@ -16,16 +16,52 @@ npm install @fwd/sheets
 yarn add @fwd/sheets
 ```
 
+## Usage
+
+```javascript
+
+const spreadsheets = require('@fwd/sheets')
+
+var config = {
+	"type": "service_account",
+	"project_id": "",
+	"private_key_id": "",
+	"private_key": "",
+	"client_email": "",
+	"client_id": "",
+	"auth_uri": "",
+	"token_uri": "",
+	"auth_provider_x509_cert_url": "",
+	"client_x509_cert_url": ""
+	"spreadsheetId": ""
+}
+
+;(async() => {
+
+	// init
+	const spreadsheet = await spreadsheets(config)
+
+	// Available methods
+	spreadsheet.addRow(worksheetName, value) {}
+	spreadsheet.addRows(worksheetName, values) {}
+	spreadsheet.getRows(worksheetName) {}
+	spreadsheet.getWorksheet(worksheetName) {}
+	spreadsheet.setWorksheet(worksheetName, headerValues) {}
+
+})()
+
+```
+
 ## Author
 
 👤  **Forward Miami**
 
 * Website: [https://forward.miami](https://forward.miami)
-* Github: [@forwardmiami](https://github.com/forwardmiami)
+* Github: [@fwd](https://github.com/fwd)
 
 ## 🤝 Contributing
 
-Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/forwardmiami/render/issues).
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/fwd/render/issues).
 
 ## Show your support
 
@@ -34,7 +70,3 @@ Give a ⭐️ if this project helped you!
 ## 📝 License
 
 Copyright © 2020 [Forward Miami](https://forward.miami).
-<br />
-<br />
-This project is [Apache--2.0](https://github.com/forwardmiami/render/blob/master/LICENSE) licensed.
-
--- index.js
+++ index.js
@@ -1,132 +1,131 @@
 const server = require('@fwd/server')
 const { GoogleSpreadsheet } = require('google-spreadsheet')
-module.exports = (config) => {
 
-  // config = {
-  //     "type": "service_account",
-  //     "project_id": "",
-  //     "private_key_id": "",
-  //     "private_key": "",
-  //     "client_email": "",
-  //     "client_id": "",
-  //     "auth_uri": "",
-  //     "token_uri": "",
-  //     "auth_provider_x509_cert_url": "",
-  //     "client_x509_cert_url": ""
-  // }
+module.exports = async (config) => {
+
+  const doc = new GoogleSpreadsheet(config.spreadsheetId);
+
+  await doc.useServiceAccountAuth(config);
+
+  await doc.loadInfo();
 
   return {
-      db: null,
-      async init(databaseId) {
-          this.db = new GoogleSpreadsheet(databaseId);
-          await this.db.useServiceAccountAuth(config);
-          return this
-      },
-      async listTitles() {
-          try {
-              var list = []
-              var worksheets = await this.list()
-              for (var i in worksheets) {
-                  list.push(worksheets[i]._rawProperties.title)
-              }
-              return list
-          } catch (e) {
-              return e.message
-          }
-      },
-      async list() {
-          try {
-              await this.db.loadInfo();
-              return this.db.sheetsById
-          } catch (e) {
-              return e.message
-          }
-      },
-      async find(worksheet) {
-          await this.db.loadInfo();
-          var list = await this.list()
-          for (index in list) {
-              if (index === worksheet || list[index]._rawProperties && list[index]._rawProperties.title === worksheet) {
-                  worksheet = list[index]
-              }
-          }
-          if (typeof worksheet === 'string') {
-              return false
-          }
-          return worksheet
-      },
-      async get(worksheet) {
-          try {
-              await this.db.loadInfo();
-              var response = []
-              worksheet = await this.find(worksheet)
-              if (!worksheet) {
-                  return false
-              }
-              var rows = await worksheet.getRows()
-              rows.map((row) => {
-                  var item = {}
-                  Object.keys(row).map(a => {
-                      if (a !== '_sheet' && a !== '_rawData') {
-                          item[a] = row[a]
-                      }
-                  })
-                  response.push(item)
-              })
-              return response
-          } catch (e) {
-              return {
-                  error: true,
-                  message: e.message
-              }
-          }
-      },
-      async add(worksheet, values) {
-          await this.db.loadInfo();
-          if (typeof worksheet === 'string') {
-              worksheet = await this.find(worksheet)
-          }
-          if (!worksheet) {
-              return false
-          }
-          values.uuid = server.uuid().slice(0, 7)
-          var row = await worksheet.addRow(values)
-          return row
-      },
-      async update(worksheet, row, change) {
-          await this.db.loadInfo()
-          if (typeof worksheet === 'string') {
-              worksheet = await this.find(worksheet)
-          }
-          if (!worksheet) {
-              return false
-          }
-          var rows = await worksheet.getRows();
-          var row = rows.find(a => a.uuid === row) || rows.find(a => a._rowNumber === row) || rows.find(a => a.username === row) || rows.find(a => a.email === row)
-          if (row) {
-              for (var i in change) {
-                  row[i] = change[i];
-              }
-              await row.save(); // save updates
-          }
-      },
-      async create(worksheet, headerValues) {
-          await this.db.loadInfo();
-          headerValues = headerValues
-          if (!headerValues.find(a => a === 'uuid')) {
-              headerValues.unshift('uuid')
-          }
-          try {
-              const newSheet = await this.db.addSheet({
-                  title: worksheet,
-                  headerValues: headerValues
-              });
-              return newSheet
-          } catch (e) {
-              console.log(e.message)
-              return
-          }
+
+    // async getHeaderValues() {},
+    // async getHeaderValues(array) {},
+
+    async getProperties() {
+      return doc
+    },
+
+    async setProperties(properties) {
+      await doc.updateProperties(properties);
+    },
+
+    async getWorksheets() {
+      var list = await doc.sheetsById
+      var worksheets = []
+      for (index in list) {
+        worksheets.push(list[index]._rawProperties)
       }
+      return worksheets
+    },
+
+    async setRow(worksheetName, rowId, object) {
+      
+      var sheet = await this.getWorksheet(worksheetName)
+
+      if (!sheet) {
+        return false
+      }
+
+      var rows = await sheet.getRows()
+      
+      var row = rows.find(a => a._rowNumber === rowId)
+
+      if (row) {
+          
+          for (var i in object) {
+              row[i] = object[i];
+          }
+
+          await row.save();
+
+      }
+
+    },
+
+    async addRow(worksheetName, value) {
+
+      var sheet = this.getWorksheet(worksheetName)
+
+      if (!sheet) {
+        return false
+      }
+
+      sheet.addRow(value)
+
+    },
+    
+    async addRows(worksheetName, values) {
+
+      var sheet = this.getWorksheet(worksheetName)
+
+      if (!sheet) {
+        return false
+      }
+
+      sheet.addRows(values)
+
+    },
+
+    async getRows(worksheetName) {
+
+      var sheet = await this.getWorksheet(worksheetName)
+
+      var rows = await sheet.getRows()
+      
+      var data = []
+
+      rows.map((row) => {
+          var item = {}
+          Object.keys(row).map(a => {
+              if (a !== '_sheet' && a !== '_rawData') {
+                  item[a] = row[a]
+              }
+          })
+          data.push(item)
+      })
+
+      return data
+
+    },
+
+    async getWorksheet(worksheetName) {
+
+      var sheets = await this.getWorksheets()
+
+      var sheet = sheets.find(a => a.title === worksheetName || a.sheetId === worksheetName)
+
+      if (!sheet) {
+        return false
+      }
+
+      return doc.sheetsById[sheet.sheetId]
+
+    },
+
+    async setWorksheet(worksheetName, headerValues) {
+
+      const newSheet = await doc.addSheet({
+          title: worksheetName,
+          headerValues: headerValues
+      });
+
+      return newSheet
+
+    },
+
   }
   
 }
